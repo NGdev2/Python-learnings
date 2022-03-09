9d997c90c4f16118388d454ad1fb2119ea5f89e0
Aidar <aydar.nizamov.98@mail.ru>
2022-03-09 23:49:27 +0300

    Add files via upload

---
+++ work_with_file.py
@@ -0,0 +1,8 @@
+f = open('text.txt', 'r')
+#print(f.read(11), '... первые 11 символов')
+for line in f:
+    print (line)
+
+# f = open('text.txt', 'w')
+# f.write('Hi, it\'s me! Luigi!\nTest')
+# f.close ()
